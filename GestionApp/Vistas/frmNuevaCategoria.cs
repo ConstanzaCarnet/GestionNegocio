45feac1f46441eeda280d88448710870d9c5f678
CarnetConstanza <carnetconstanza@gmail.com>
2026-04-21 17:23:08 -0300

    Changes in methods

---
+++ GestionApp/Vistas/frmNuevaCategoria.cs
@@ -0,0 +1,53 @@
+﻿using GestionApp.src.DTOs.Request;
+using GestionApp.src.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionApp.Vistas
+{
+    public partial class frmNuevaCategoria : Form
+    {
+        public frmNuevaCategoria()
+        {
+            InitializeComponent();
+        }
+        //servicio
+        private CategoríaService _categoriaService = new CategoríaService();
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "")//la descripcion no es informacion obligatoria
+            {
+                cmdCrear.Enabled = false;
+            }
+            else
+            {
+                cmdCrear.Enabled = true;
+            }
+        }
+
+        private void cmdCrear_Click(object sender, EventArgs e)
+        {
+            string descripcion = txtDescripcion.Text;
+            if (descripcion == null) descripcion = "Sin descripcion";
+            //instancio categoriadto
+            var categoria = new CategoriaDto
+            {
+                Nombre = txtNombre.Text,
+                Descripcion = txtDescripcion.Text
+            };
+            //llamo la funcion del servicio
+            _categoriaService.Crear(categoria);
+            MessageBox.Show("Categoría creada con éxito", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //limpio formulario
+            txtDescripcion.Text = "";
+            txtNombre.Text = "";
+        }
+    }
+}
---
+++ GestionApp/src/DTOs/Response/MovimientoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionApp.src.DTOs.Response
+{
+    public class MovimientoDto
+    {
+        public int IdMovimiento {get; set;}
+        public DateTime Fecha { get; set; }
+        public string Tipo { get; set; }
+        public decimal Monto { get; set; }
+        public string Detalle { get; set; }
+    }
+}
